7cca3afc11fe8cac842c83ef137462eefb820385
kksmash123 <kksmashes@gmail.com>
2022-08-21 16:06:38 +0530

    recursion basic

--- recursion_tutorial/recursion_tutorial.cpp
+++ recursion_tutorial/recursion_tutorial.cpp
@@ -1,20 +1,66 @@
-// recursion_tutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+
+
+
+int power2(int m, int n)
+{
+
+	if (n == 0)
+		return 1;
+	else if (n % 2 == 0)
+		return power2(m * m, n / 2);
+	else
+		return power2(m * m, n / 2) * m;
+
+}
+
+
+int power(int m, int n)
+{
+	if (n == 0)
+		return 1;
+	return power(m, n - 1)* m;
+}
+
+int fact(int n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return fact(n - 1) * n;
+}
+
+int fun1(int n)
+{
+	if (n == 0)
+		return 0;
+	else
+		return fun1(n - 1) + n;
+
+
+}
+
+int fun(int a)
+{
+	static int sum = 0;
+
+	if (a > 0)
+	{
+		sum += a;
+		fun(a - 1);
+
+	}
+	return sum;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//cout<<fact(5);
+
+	cout << power2(2, 10);
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
